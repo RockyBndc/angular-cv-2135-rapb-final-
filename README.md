1b8df223878561ab35702cf13e194f82b02ca1ed
RockyBndc <123609583+RockyBndc@users.noreply.github.com>
2023-02-04 11:38:18 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-cv-2135-rapb-final-
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-cv-2135-rapb)
---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ReferenceComponent } from './reference/reference.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, AboutComponent, EducationComponent, SkillsComponent, ReferenceComponent, ContactComponent],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
